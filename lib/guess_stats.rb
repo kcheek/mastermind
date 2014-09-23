5a3bf6a2864e9704cc5d648433dbedd4309f9fc6
Kalah Cheek <kcheek7@ymail.com>
2014-09-22 22:04:05 -0600

    GuessChecker is now GuessStats
    Add a file and a test file showing how it is being used

---
+++ lib/guess_stats.rb
@@ -0,0 +1,2 @@
+class GuessStats
+end
---
+++ test/test_guess_stats.rb
@@ -0,0 +1,4 @@
+# puts "Your guess of #{stats.guess.inspect}"
+# puts "has #{stats.correct_spot} colors in the correct spot"
+# puts "and #{stats.incorrect_spot} colors in the incorrect spot"
+# puts "and #{stats.incorrect_color} colors that are incorrect"
